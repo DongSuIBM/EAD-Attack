a7e2d5617bf7e24f37c28aa494716dc22947b22c
chicago <dong.su@ibm.com>
2017-11-05 14:38:16 -0500

    adding more print statements to show algorithm details

--- l1_attack.py
+++ l1_attack.py
@@ -9,6 +9,7 @@
 import sys
 import tensorflow as tf
 import numpy as np
+import time
 
 BINARY_SEARCH_STEPS = 9  # number of times to adjust the constant with binary search
 MAX_ITERATIONS = 10000   # number of iterations to perform gradient descent
@@ -94,18 +95,18 @@ class EADL1:
         self.elasticdist_y = self.l2dist_y + tf.multiply(self.l1dist_y, self.beta_t)
         
         # compute the probability of the label class versus the maximum other
-        real = tf.reduce_sum((self.tlab)*self.output,1)
-        real_y = tf.reduce_sum((self.tlab)*self.output_y,1)
-        other = tf.reduce_max((1-self.tlab)*self.output - (self.tlab*10000),1)
-        other_y = tf.reduce_max((1-self.tlab)*self.output_y - (self.tlab*10000),1)
+        self.real = tf.reduce_sum((self.tlab)*self.output,1)
+        self.real_y = tf.reduce_sum((self.tlab)*self.output_y,1)
+        self.other = tf.reduce_max((1-self.tlab)*self.output - (self.tlab*10000),1)
+        self.other_y = tf.reduce_max((1-self.tlab)*self.output_y - (self.tlab*10000),1)
         if self.TARGETED:
             # if targeted, optimize for making the other class most likely
-            loss1 = tf.maximum(0.0, other-real+self.CONFIDENCE)
-            loss1_y = tf.maximum(0.0, other_y-real_y+self.CONFIDENCE)
+            loss1 = tf.maximum(0.0, self.other-self.real+self.CONFIDENCE)
+            loss1_y = tf.maximum(0.0, self.other_y-self.real_y+self.CONFIDENCE)
         else:
             # if untargeted, optimize for making this class least likely.
-            loss1 = tf.maximum(0.0, real-other+self.CONFIDENCE)
-            loss1_y = tf.maximum(0.0, real_y-other_y+self.CONFIDENCE)
+            loss1 = tf.maximum(0.0, self.real-self.other+self.CONFIDENCE)
+            loss1_y = tf.maximum(0.0, self.real_y-self.other_y+self.CONFIDENCE)
 
         # sum up the losses
         self.loss21 = tf.reduce_sum(self.l1dist)
@@ -167,6 +168,7 @@ class EADL1:
         lower_bound = np.zeros(batch_size)
         CONST = np.ones(batch_size)*self.initial_const
         upper_bound = np.ones(batch_size)*1e10
+        n_success = 0
 
         # the best l2, score, and image attack
         o_bestl1 = [1e10]*batch_size
@@ -194,44 +196,91 @@ class EADL1:
             self.sess.run(self.setter, feed_dict={self.assign_newimg: batch})
             self.sess.run(self.setter_y, feed_dict={self.assign_slack: batch})
             prev = 1e6
+            train_timer = 0.0
             for iteration in range(self.MAX_ITERATIONS):
+                # print out the losses every 10%
+                if iteration%(self.MAX_ITERATIONS//self.print_every) == 0:
+                    # print(iteration,self.sess.run((self.loss,self.real,self.other,self.loss1,self.loss2)))
+                    # grad = self.sess.run(self.grad_op)
+                    # old_modifier = self.sess.run(self.modifier)
+                    # np.save('white_iter_{}'.format(iteration), modifier)
+                    loss, real, other, loss1, loss2 = self.sess.run((self.loss,self.real,self.other,self.loss1,self.loss2))
+                    print("loss = ", loss)
+                    print("real = ", real)
+                    print("other = ", other)
+                    print("loss1 = ", loss1)
+                    print("loss2 = ", loss2)
+
+                    if self.batch_size == 1:
+                        print("[STATS][L2] iter = {}, time = {:.3f}, loss = {:.5g}, real = {:.5g}, other = {:.5g}, loss1 = {:.5g}, loss2 = {:.5g}".format(iteration, train_timer, loss[0], real[0], other[0], loss1[0], loss2[0]))
+                    elif self.batch_size > 10:
+                        # print("[STATS][L2][SUM of {}] iter = {}, time = {:.3f}, batch_size = {}, n_success = {:.5g}, loss = {:.5g}, real = {:.5g}, other = {:.5g}, loss1 = {:.5g}, loss2 = {:.5g}".format(self.batch_size, iteration, train_timer, batch_size, n_success, sum(loss), sum(real), sum(other), sum(loss1), sum(loss2)))
+                        print("[STATS][L2][SUM of {}] iter = {}, time = {:.3f}, batch_size = {}, n_success = {:.5g}, real = {:.5g}, other = {:.5g}".format(self.batch_size, iteration, train_timer, batch_size, n_success, sum(real), sum(other)))
+                    else:
+                        print("[STATS][L2] iter = {}, time = {:.3f}".format(iteration, train_timer))
+                        print("[STATS][L2] real =", real)
+                        print("[STATS][L2] other =", other)
+                        print("[STATS][L2] loss1 =", loss1)
+                        print("[STATS][L2] loss2 =", loss2)
+                        print("[STATS][L2] loss =", loss)
+                    sys.stdout.flush()
+
+                attack_begin_time = time.time()
+
                 # perform the attack 
                 self.sess.run([self.train])
                 self.sess.run([self.setter, self.setter_y])
                 l, l2s, l1s, elastic, scores, nimg = self.sess.run([self.loss, self.l2dist, self.l1dist, self.elasticdist, self.output, self.newimg])
 
 
-
                 # print out the losses every 10%
-                """
-                if iteration%(self.MAX_ITERATIONS//10) == 0:
-                    print(iteration,self.sess.run((self.loss,self.loss1,self.loss2,self.loss21)))
-                """
+                # if iteration%(self.MAX_ITERATIONS//10) == 0:
+                print(iteration,self.sess.run((self.loss,self.loss1,self.loss2,self.loss21)))
+
                 # check if we should abort search if we're getting nowhere.
                 if self.ABORT_EARLY and iteration%(self.MAX_ITERATIONS//10) == 0:
                     if l > prev*.9999:
-						print("Early stopping because there is no improvement")
+                        print("Early stopping because there is no improvement")
                         break
                     prev = l
 
                 # adjust the best result found so far
+                read_last_loss = False
                 for e,(l1,sc,ii) in enumerate(zip(l1s,scores,nimg)):
                     if l1 < bestl1[e] and compare(sc, np.argmax(batchlab[e])):
                         bestl1[e] = l1
                         bestscore[e] = np.argmax(sc)
                     if l1 < o_bestl1[e] and compare(sc, np.argmax(batchlab[e])):
+                        # print a message if it is the first attack found
+                        if o_bestl1[e] == 1e10:
+                            if not read_last_loss: 
+                                #loss, real, other, loss1, loss2 = self.sess.run((self.loss,self.real,self.other,self.loss1,self.loss2))
+                                real, other = self.sess.run((self.real,self.other))
+                                read_last_loss = True
+                            #print("[STATS][L3][First valid attack found!] iter = {}, time = {:.3f}, img = {}, loss = {:.5g}, real = {:.5g}, other = {:.5g}, loss1 = {:.5g}, loss2 = {:.5g}".format(iteration, train_timer, e, loss[e], real[e], other[e], loss1[e], loss2[e]))
+                            print("[STATS][L3][First valid attack found!] iter = {}, time = {:.3f}, img = {}, real = {:.5g}, other = {:.5g}".format(iteration, train_timer, e, real[e], other[e]))
+
+                            n_success += 1
                         o_bestl1[e] = l1
                         o_bestscore[e] = np.argmax(sc)
                         o_bestattack[e] = ii
+                        o_best_const[e] = CONST[e]
+
+                train_timer += time.time() - attack_begin_time
 
             # adjust the constant as needed
             for e in range(batch_size):
                 if compare(bestscore[e], np.argmax(batchlab[e])) and bestscore[e] != -1:
+                    # modifier = self.sess.run(self.modifier)
+                    # np.save("best.model", modifier)
+                    print('old constant: ', CONST[e])
                     # success, divide const by two
                     upper_bound[e] = min(upper_bound[e],CONST[e])
                     if upper_bound[e] < 1e9:
                         CONST[e] = (lower_bound[e] + upper_bound[e])/2
+                    print('new constant: ', CONST[e])
                 else:
+                    print('old constant: ', CONST[e])
                     # failure, either multiply by 10 if no solution found yet
                     #          or do binary search with the known upper bound
                     lower_bound[e] = max(lower_bound[e],CONST[e])
@@ -239,6 +288,7 @@ class EADL1:
                         CONST[e] = (lower_bound[e] + upper_bound[e])/2
                     else:
                         CONST[e] *= 10
+                    print('new constant: ', CONST[e])
 
         # return the best solution found
         o_bestl1 = np.array(o_bestl1)
